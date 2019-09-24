7c786c77b96d12076990c6cc2d85ed9d33db8a96
johndoejagg <john.doe.jagg@gmail.com>
2019-09-24 12:07:30 +0200

    beautify lang.php

--- n-admin/language/de.php
+++ n-admin/language/de.php
@@ -1,10 +1,24 @@
 <?php
+
   $lang=new \stdClass();
   $lang->logout="ausloggen";
+  $lang->save="speichern";
+  $lang->cancel="abbrechen";
+  $lang->description="Beschreibung";
+  $lang->saveDraft="Entwurf speichern";
+  $lang->categorys="Kategorien";
+
   $lang->menu=array(
-    array("Blog","blog"),
-    array("Seiten","pages"),
-    array("Medien","media"),
-    array("Einstellungen","settings",array(array("Themes","themes"),array("User","user"),array("Bildformat","imageformat"),array("E-mail","mail")))
-  )
+    array("Blog", "blog"),
+    array("Seiten", "pages"),
+    array("Medien", "media"),
+    array("Einstellungen"," settings", array(
+                                      array("Themes","themes"),
+                                      array("User","user"),
+                                      array("Bildformat","imageformat"),
+                                      array("E-mail","mail")
+                                    )
+        )
+  );
+
 ?>
