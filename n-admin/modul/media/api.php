e2901e9980bd76ada8eaf57fe11293caa8737e81
lukas wallmann <john.doe.jagg@gmail.com>
2020-09-02 18:45:48 +0200

    work on filebrowser

--- n-admin/modul/media/api.php
+++ n-admin/modul/media/api.php
@@ -3,7 +3,7 @@
   define("BROWSDIR",$_GET["dir"]);
 
   function listDir($dir){
-      echo "dir:",$dir."\n";
+      echo "dir:",substr(realpath("upload/".$dir), strlen(realpath("upload/"))+1)."\n";
       if ($handle = opendir("upload/".$dir)) {
 
         while (false !== ($entry = readdir($handle))) {
@@ -16,6 +16,39 @@
 
         closedir($handle);
     }
+  }
+
+  function getRelativePath($from, $to)
+  {
+      // some compatibility fixes for Windows paths
+      $from = is_dir($from) ? rtrim($from, '\/') . '/' : $from;
+      $to   = is_dir($to)   ? rtrim($to, '\/') . '/'   : $to;
+      $from = str_replace('\\', '/', $from);
+      $to   = str_replace('\\', '/', $to);
+
+      $from     = explode('/', $from);
+      $to       = explode('/', $to);
+      $relPath  = $to;
+
+      foreach($from as $depth => $dir) {
+          // find first non-matching dir
+          if($dir === $to[$depth]) {
+              // ignore this directory
+              array_shift($relPath);
+          } else {
+              // get number of remaining dirs to $from
+              $remaining = count($from) - $depth;
+              if($remaining > 1) {
+                  // add traversals up to first matching dir
+                  $padLength = (count($relPath) + $remaining - 1) * -1;
+                  $relPath = array_pad($relPath, $padLength, '..');
+                  break;
+              } else {
+                  $relPath[0] = './' . $relPath[0];
+              }
+          }
+      }
+      return implode('/', $relPath);
   }
 
   function createDir(){
--- n-admin/modul/media/index.php
+++ n-admin/modul/media/index.php
@@ -15,26 +15,32 @@
       }else{
         fileBrowser.rebuildDirStructure(entrys[0].split("dir:")[1]);
         var html=[];
-        for(var i=1; i<entrys.length; i++){
+        for(var i=1; i<entrys.length-1; i++){
           html.push(fileBrowser.createElement(entrys[i]));
         }
-        $("#fileBrowser .browser").html(html);
-
+        $("#fileBrowser .browser").html(html.join(""));
+        $("#fileBrowser .entry.folder").click(function(){
+          fileBrowser.dir=fileBrowser.dir+"/"+$(this).find(".name").text();
+          fileBrowser.reloadList();
+        })
       }
     },
     createElement:function(name){
       if(name==".."||name.split(".").length==1){
-        return "<div class='entry'><div class='icon folder'></div><div class='name'>"+name+"</div></div>";
+        if(name!=".."){
+          return "<div class='entry folder'><div class='icon'></div><div class='name'>"+name+"</div></div>";
+
+        }else if(fileBrowser.dir!="")return "<div class='entry folder'><div class='icon'></div><div class='name'>"+name+"</div></div>";
       }else{
         var ext=name.split(".")[name.split(".").length-1];
-        
+
       }
     },
 
     rebuildDirStructure:function(dir){
-      alert("listing:"+dir)
       fileBrowser.dir=dir;
       var parts=dir.split("/");
+      $("#fileBrowser .address").html("");
       $("#fileBrowser .address").append("<span>upload<span>");
       for(var i=0; i<parts.length; i++){
         $("#fileBrowser .address").append("<span>"+parts[i]+"<span>");
@@ -48,6 +54,27 @@
     fileBrowser.init();
   });
 </script>
+<style>
+  #fileBrowser .entry{
+    display: inline-block;
+    width: 200px;
+    height:220px;
+    position: relative;
+    margin:0 15px 15px 0;
+    border: 1px solid #ccc;
+    box-shadow: 0 0 5px rgba(0, 0, 0, 0.3);
+    cursor:pointer;
+  }
+  #fileBrowser .entry .name{
+    position: absolute;
+    width: 200px;
+    bottom:0px;
+    color:#fff;
+    left:0px;
+    padding:0 5px;
+    background:rgba(0,0,0,0.7);
+  }
+</style>
 <div id="fileBrowser">
   <div class="nav">
     <span class="add"></span>
